54b9fc506cd468cccc5fe1eaf4d0f206f7214e6d
Wildan Muhammad Athoillah <wildantechnoads77@gmail.com>
2025-04-25 00:41:57 +0700

    Setup Project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,11 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.google.devtools.ksp)
+    kotlin("kapt")
+    id("androidx.navigation.safeargs.kotlin")
+    id("dagger.hilt.android.plugin")
+    id("kotlin-parcelize")
 }
 
 android {
@@ -15,6 +20,9 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        setProperty("archivesBaseName", "Nusantara Space-v$versionCode($versionName) ")
+        vectorDrawables.useSupportLibrary = true
+        multiDexEnabled = true
     }
 
     buildTypes {
@@ -26,12 +34,19 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
@@ -40,8 +55,38 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.swiperefreshlayout)
+    implementation(libs.androidx.multidex)
+    implementation(libs.androidx.paging)
+    implementation (libs.androidx.room.paging)
+    implementation (libs.androidx.room.ktx)
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
+    annotationProcessor(libs.androidx.room.compiler)
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.github.razir.progressbutton)
+    implementation(libs.androidx.datastore)
+    implementation(libs.androidx.vectordrawable)
+    implementation(libs.github.bumptech.glide)
     implementation(libs.androidx.activity)
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
+    implementation(libs.com.facebook.shimmer)
+    ksp(libs.github.bumptech.ksp)
+    implementation(libs.hilt.android) // Hilt dependencies
+    kapt(libs.hilt.android.compiler) // Hilt annotation processor
+    implementation(libs.androidx.lifecycle.common)
+    implementation(libs.androidx.lifecycle.viewmodel)
+    implementation(libs.androidx.lifecycle.livedata)
+    implementation(libs.androidx.lifecycle.runtime)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.com.squareup.retrofit)
+    implementation(libs.com.squareup.gson)
+    implementation(libs.com.squareup.rxjava2)
+    implementation(libs.org.jetbrains.serialization)
+    implementation(libs.squareup.okhttp3.logging)
+    implementation(libs.google.code.gson)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
